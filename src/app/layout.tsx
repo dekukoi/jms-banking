8bfbb2580d7ea62b9fdeb53ff089ef8fb65a12cd
Koily <171057947+DredIvany@users.noreply.github.com>
2026-03-20 14:00:12 +0700

    minor setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,3 +20,18 @@ export default function RootLayout({
     </html>
   );
 }
+
+/* Sentry only
+      import * as Sentry from '@sentry/nextjs';
+      import type { Metadata } from 'next';
+
+      // Add or edit your "generateMetadata" to include the Sentry trace data:
+      export function generateMetadata(): Metadata {
+        return {
+          // ... your existing metadata
+          other: {
+            ...Sentry.getTraceData()
+          }
+        };
+      }
+*/
