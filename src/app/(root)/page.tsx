ecaad060b2ee2cad942d0b06f41bc70eb6b5cb1f
Koily <171057947+DredIvany@users.noreply.github.com>
2026-03-22 01:51:24 +0700

    Home Page: add header box

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: "John" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            //#region Why use optional chaining (?.) here?            
+            // If loggedIn exists → access .firstName
+            // If loggedIn is null or undefined → return undefined instead of throwing an error
+
+            // This is used where loggedIn might be null (e.g. user is not authenticated yet, or data is still loading from an API).
+           //#endregion
+            subtext="This is a subtitle"
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
